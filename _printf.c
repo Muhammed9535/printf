f537ffd576ac8aec6c8adcbe50b05ca919592321
Muhammed9535 <lawalolarewaju123@gmai.com>
2024-01-29 05:31:50 -0800

    1st commit

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * _printf - a function that produces output according to a format.
+  * @format: is a character string
+  * @...: variable number of argument
+  * Return: number of character printed
+  */
+int _printf(const char *format, ...)
+{
+	va_list list_arg;
+
+	va_start(list_arg, format);
+
+}
