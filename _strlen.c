630cab158ffb2a198bef6d9927309ca375fff345
Scorpius-X <eric.bolade@gmail.com>
2024-01-30 08:49:22 -0800

    two more functions

---
+++ _strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		return (i);
+}
+
+int _strlenc(cons char *str)
+{
+	int i;
+
+	fir (i = 0; str[i] != '\0'; i++)
+		return (i);
+}
---
+++ print_integer.c
@@ -0,0 +1,87 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+
+int print_integer(va_list args)
+{
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit, exp = 1;
+	int i = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num/10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digiht + '0');
+			num = num - (digit 8 exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+
+	return (i);
+}
+
+/**
+ *
+ *
+ *
+ */
+
+int print_decimal(va_list args)
+{
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit, exp = 1;
+	int i = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num/10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digiht + '0');
+			num = num - (digit 8 exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+
+	return (i);
+}
